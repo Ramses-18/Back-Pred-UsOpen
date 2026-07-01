0b397bd4ae7ae685d19f11ee4836f9a95463a1d0
“Ramses-18” <tomyvladimir18@gmail.com>
2026-07-01 12:01:00 -0300

    anadiendo actualizacion

--- src/main/java/com/wimbledon/service/MatchAdminService.java
+++ src/main/java/com/wimbledon/service/MatchAdminService.java
@@ -33,18 +33,55 @@ public class MatchAdminService {
         res.setSetsWinner(dto.getSetsWinner());
         res.setSetsLoser(dto.getSetsLoser());
         res.setGameResult(dto.getGameResult());
-
+        
         // Sets individuales
-        res.setSet1W(dto.getSet1W()); res.setSet1L(dto.getSet1L());
-        res.setSet2W(dto.getSet2W()); res.setSet2L(dto.getSet2L());
-        res.setSet3W(dto.getSet3W()); res.setSet3L(dto.getSet3L());
-        res.setSet4W(dto.getSet4W()); res.setSet4L(dto.getSet4L());
-        res.setSet5W(dto.getSet5W()); res.setSet5L(dto.getSet5L());
+        res.setSet1W(dto.getSet1W() != null ? dto.getSet1W() : 0); 
+        res.setSet1L(dto.getSet1L() != null ? dto.getSet1L() : 0);
+        res.setSet2W(dto.getSet2W() != null ? dto.getSet2W() : 0); 
+        res.setSet2L(dto.getSet2L() != null ? dto.getSet2L() : 0);
+        res.setSet3W(dto.getSet3W() != null ? dto.getSet3W() : 0); 
+        res.setSet3L(dto.getSet3L() != null ? dto.getSet3L() : 0);
+        res.setSet4W(dto.getSet4W() != null ? dto.getSet4W() : 0); 
+        res.setSet4L(dto.getSet4L() != null ? dto.getSet4L() : 0);
+        res.setSet5W(dto.getSet5W() != null ? dto.getSet5W() : 0); 
+        res.setSet5L(dto.getSet5L() != null ? dto.getSet5L() : 0);
 
         res.setEnteredAt(LocalDateTime.now());
         resultRepo.save(res);
         return dto;
     }
+
+/* 
+    public MatchResultDto saveResult(Long matchId, MatchResultDto dto) {
+        MatchResult result = MatchResult.builder()
+            .matchId(matchId)
+            .winner(dto.getWinner())
+            .setsWinner(dto.getSetsWinner())
+            .setsLoser(dto.getSetsLoser())
+            .gameResult(dto.getGameResult())
+            .enteredAt(LocalDateTime.now())
+            // Protección contra nulos: Si el set es null, guardamos 0
+            .set1W(dto.getSet1W() != null ? dto.getSet1W() : 0)
+            .set1L(dto.getSet1L() != null ? dto.getSet1L() : 0)
+            .set2W(dto.getSet2W() != null ? dto.getSet2W() : 0)
+            .set2L(dto.getSet2L() != null ? dto.getSet2L() : 0)
+            .set3W(dto.getSet3W() != null ? dto.getSet3W() : 0)
+            .set3L(dto.getSet3L() != null ? dto.getSet3L() : 0)
+            .set4W(dto.getSet4W() != null ? dto.getSet4W() : 0)
+            .set4L(dto.getSet4L() != null ? dto.getSet4L() : 0)
+            .set5W(dto.getSet5W() != null ? dto.getSet5W() : 0)
+            .set5L(dto.getSet5L() != null ? dto.getSet5L() : 0)
+            .build();
+        
+        resultRepo.save(result);
+        return dto;
+    }
+*/
+   
+
+
+
+
 
     public void deleteMatch(Long id) { matchRepo.deleteById(id); }
 }

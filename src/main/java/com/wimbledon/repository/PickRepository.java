4287a0f3a94b57acccc6bcee472dc3b0aacc0035
“Ramses-18” <tomyvladimir18@gmail.com>
2026-07-03 02:02:36 -0300

    ComeBack 2

--- src/main/java/com/wimbledon/repository/PickRepository.java
+++ src/main/java/com/wimbledon/repository/PickRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
 public interface PickRepository extends JpaRepository<Pick, Long> {
-    List<Pick> findByUserId(Long userId);
+    Optional<Pick> findByUserIdAndMatchId(Long userId, Long matchId);
     @Query("SELECT p FROM Pick p WHERE p.match.id = :matchId")
     List<Pick> findByMatchId(Long matchId);
     @Query("SELECT p FROM Pick p WHERE p.user.id = :userId AND p.match.matchDate = CURRENT_DATE")

5ea150a9b576c25088d9218db1d5ad14fb40f731
Z User <z@container>
2026-07-07 15:59:55 +0000

    fix: add missing Async import in NotificationService

--- src/main/java/com/wimbledon/service/NotificationService.java
+++ src/main/java/com/wimbledon/service/NotificationService.java
@@ -9,6 +9,7 @@ import com.wimbledon.repository.PickRepository;
 import com.wimbledon.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
